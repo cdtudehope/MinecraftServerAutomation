1eb015732e2e367ba563b9509e969e522d36b943
Minecraft Server <minecraftserver@Chriss-MacBook-Pro-2.local>
2018-07-30 20:47:33 -0500

    Change over to being used on new mac computer

--- MinecraftBackup.py
+++ MinecraftBackup.py
@@ -6,8 +6,8 @@ import time
 from Backup import backupServer
 
 #Start of program
-backupSrc = "E:\\ServerForThePlex\\world"	
-backupDest = "C:\\MinecraftBackups\\"
+backupSrc = "/Users/minecraftserver/Desktop/Minecraft/world"	
+backupDest = "/Users/minecraftserver/Desktop/MinecraftBackups/"
 print("Minecraft Backup is in Progress. It takes awhile :(")
 if backupServer(backupSrc,backupDest,15) == True:	#Check if backup occurred
 	print("A backup was created in the folder: " + backupDest)
